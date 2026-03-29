329136265e7d1eb68d4c8440623cb8b041017d3e
bhati46 <dakshbhati2900@gmail.com>
2026-03-29 22:58:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Table Difference/table-difference.py
@@ -0,0 +1,14 @@
+#User function Template for python3
+
+def difference(n1,n2):
+    #Complete the code below
+    i=1
+    j=1
+    while(i<=10):
+        x=n1*i
+        y=n2*j
+        i+=1
+        j+=1
+        print(x-y,end=" ")
+        
+        
